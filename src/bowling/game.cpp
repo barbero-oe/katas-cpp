19100affe4604489a9a434f75a71ca57f6f7a761
Ezequiel Barbero <barbero.oe@gmail.com>
2022-06-05 21:54:18 -0300

    Calculates the strike

--- src/bowling/game.cpp
+++ src/bowling/game.cpp
@@ -11,13 +11,18 @@ namespace bowling
         auto frame_cursor = _rolls.begin();
         for (int frame = 0; frame < 10; frame++)
         {
-            if (is_spare(frame_cursor)) {
-                score += score_spare(frame_cursor);
-                frame_cursor += 2;
-            } else if (is_strike(frame_cursor)) {
+            if (is_strike(frame_cursor))
+            {
                 score += score_strike(frame_cursor);
                 frame_cursor += 1;
-            } else {
+            }
+            else if (is_spare(frame_cursor))
+            {
+                score += score_spare(frame_cursor);
+                frame_cursor += 2;
+            }
+            else
+            {
                 score += score_simple(frame_cursor);
                 frame_cursor += 2;
             }
@@ -25,23 +30,28 @@ namespace bowling
         return score;
     }
 
-    int Game::score_simple(const vector<int>::const_iterator& it) {
+    int Game::score_simple(const vector<int>::const_iterator &it)
+    {
         return *it + *(it + 1);
     }
 
-    bool Game::is_spare(const vector<int>::const_iterator& it) {
+    bool Game::is_spare(const vector<int>::const_iterator &it)
+    {
         return *it + *(it + 1) == 10;
     }
 
-    int Game::score_spare(const vector<int>::const_iterator& it) {
+    int Game::score_spare(const vector<int>::const_iterator &it)
+    {
         return 10 + *(it + 2);
     }
 
-    bool Game::is_strike(const vector<int>::const_iterator& it) {
+    bool Game::is_strike(const vector<int>::const_iterator &it)
+    {
         return *it == 10;
     }
 
-    int Game::score_strike(const vector<int>::const_iterator& it) {
+    int Game::score_strike(const vector<int>::const_iterator &it)
+    {
         return 10 + *(it + 1) + *(it + 2);
     }
 
--- tests/bowling_tests.cpp
+++ tests/bowling_tests.cpp
@@ -1,4 +1,5 @@
 #include "../src/bowling/game.hpp"
+#include <iostream>
 #include <catch2/catch_test_macros.hpp>
 
 void roll_times(bowling::Game &game, int value, int times)
@@ -52,4 +53,35 @@ TEST_CASE("A new bowling game", "[bowling]")
         game.roll(1);
         REQUIRE(12 == game.score());
     }
+    SECTION("when a play is made the calculation is right") {
+        game.roll(1);
+        game.roll(4);
+        game.roll(4);
+        game.roll(5);
+        game.roll(6);
+        game.roll(4);
+        game.roll(5);
+        game.roll(5);
+        game.roll(10);
+        game.roll(0);
+        game.roll(1);
+        game.roll(7);
+        game.roll(3);
+        game.roll(6);
+        game.roll(4);
+        game.roll(10);
+        game.roll(2);
+        game.roll(8);
+        game.roll(6);
+        REQUIRE(133 == game.score());
+    }
+    // SECTION("when a play is made the calculation is right 2") {
+    //     roll_times(game, 4, 6);
+    //     game.roll(10);
+    //     // game.roll(5);
+    //     // game.roll(10);
+    //     roll_times(game, 4, 10);
+    //     // roll_times(game, 0, 20 - 8);
+    //     REQUIRE(24 + 10 + 4 + 4 + 44 == game.score());
+    // }
 }
